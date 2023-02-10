3dcea15393533475119531be308cbe54fda9fe43
Joseph Maa <josephgmaa@berkeley.edu>
2023-02-09 18:23:34 -0800

    Everything is broken

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Chess } from "chess.js";
 import Chessboard from "chessboardjsx";
+import { Move } from "chess.js";
 
 const App: React.FC = () => {
+  const [chess] = useState<Chess>(
+    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+  );
+
+  const [fen, setFen] = useState(chess.fen());
+
+  const handleMove = (move: Move) => {
+    console.log(move);
+    setTimeout(() => {
+      const moves = chess.moves();
+      console.log(moves);
+
+      if (moves.length > 0) {
+        const computerMove = moves[Math.floor(Math.random() * moves.length)];
+        console.log(computerMove);
+        chess.move(computerMove);
+        setFen(chess.fen());
+      }
+    }, 300);
+
+    console.log(chess.fen());
+
+    setFen(chess.fen());
+  };
+
+  // window.setTimeout(handleMove, 500);
+
   return (
     <div className="flex-center">
       <h1>Random Chess</h1>
       <Chessboard
         width={400}
-        position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        position={fen}
       />
     </div>
   );
+  // return <div></div>;
 };
 
 export default App;
